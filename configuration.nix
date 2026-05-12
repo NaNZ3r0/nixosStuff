b5a632cd53770feab93f81c1e5a1609068a1b659
mira <mira.franke@stud.h-da.de>
2026-05-12 08:40:23 +0200

    config kleiner gemacht

--- configuration.nix
+++ configuration.nix
@@ -60,7 +60,6 @@ in
   environment.systemPackages = with pkgs; [
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   git
-  vscode
   wget
   unstable.go
   flatpak
